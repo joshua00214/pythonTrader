a15c57828b3fb8a44237da6bec7b980286ac25f8
joshua00214 <stewartjoshua11@gmail.com>
2019-05-15 15:27:29 -0400

    adding the EMA indicator

---
+++ indicators/EMA.py
@@ -0,0 +1,52 @@
+from .Indicators import Indicator
+
+#EMA = price(t) * k + EMA(y)*(1-k)
+'''
+t = today
+y = yesterday
+N = number of days in the ema (maybe also use number of hours if this doesn't work)
+k = 2/(N+1)
+y = yesterday
+
+'''
+class EMA(Indicator):
+    def __init__(self, currency, length):
+        self.values = []
+        self.averages = []
+        self.length = length
+        self.k = 2 / (length + 1)
+        super().__init__(currency)
+    
+    def update(self, value):
+        #adding next value to list of values
+        
+
+
+        self.values.insert(0, value)
+        #keeping its length to correct amount
+        if len(self.values) > self.length:
+            self.values.pop()
+
+        #calculating average
+        #since EMA depends on preious ema, adding in a case for the begining of execution
+        if len(self.averages) < 2:
+            self.averages.insert(0, value)
+            return
+        #the formula for ema
+        average = (value * self.k) + (self.averages[1] *(1 - self.k))
+        #inserting the average
+        self.averages.insert(0, average)
+        if len(self.averages) > self.length:
+            self.averages.pop()
+
+    def getAverages(self):
+        if len(self.averages) == self.length:
+            return self.averages
+        else:
+            return []
+    def getValue(self):
+        if len(self.averages) == self.length:
+            return self.averages[0]
+
+    def getLength(self):
+        return self.length
